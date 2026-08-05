bc8114385b5e70eabf454944d5716081766c77df
ironGursimar <gursimar.s@vcreatek.com>
2026-08-05 20:36:00 +0530

    Update hello.py

--- week1-foundations/hello.py
+++ week1-foundations/hello.py
@@ -4,9 +4,13 @@ import sys
 # Create a Python program that prints my name and
 # displays the Python version currently being used.
 
-
+def main():
     # Print my name
     print("Name: Gursimar")
 
     # Print the Python interpreter version
     print(f"Python Version: {sys.version}")
+
+
+if __name__ == "__main__":
+    main()
